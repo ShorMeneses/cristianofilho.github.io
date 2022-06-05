16f9fa63a7ce22a92073ac212bcb1708662ef4d3
Cristiano Filho ✅ <54041918+CristianoFIlho@users.noreply.github.com>
2022-06-05 11:27:01 -0300

    button dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,24 @@ window.addEventListener('scroll', scrollActive)
 
 
 /*=============== LIGHT DARK THEME ===============*/ 
+const chk = document.getElementById('chk');
 
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
